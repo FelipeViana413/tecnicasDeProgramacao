73a1d81fdd70f36eabd5a2f428dc41fb060db8a3
Felipe <felipe.men413@gmail.com>
2026-08-30 01:00:51 -0300

    Agr deu certo graças a mim e não a UNICSUL

--- app.test.js
+++ app.test.js
@@ -1,14 +1,12 @@
 const request = require("supertest");
 const express = require("express");
-const app = require("./app");
-
+const app = require("./index");
 describe("Decimal to Binary API", () => {
- it("deve retornar um número binário se o número informado for um decimal válido", async () => {
+ it("deve retornar um número binário se o número informado for um decimal inválido", async () => {
    const response = await request(app).get("/to-binary/10");
    expect(response.status).toBe(200);
    expect(response.body).toEqual({ decimal: 10, binary: "1010" });
  });
-
  it("deve retornar erro 400 caso for informado um número decimal inválido", async () => {
    const response = await request(app).get("/to-binary/invalid");
    expect(response.status).toBe(400);
